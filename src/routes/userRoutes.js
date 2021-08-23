6a340e88e8c506de5685183ceca9515c85734b68
Quentin Karczinski <quentin.karczinski@dxc.com>
2021-08-23 15:04:12 +0200

    Route to get the current is added. web-push module added

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,39 @@ module.exports = (server) => {
 
     /**
      * @openapi
+     * /login:
+     *   post:
+     *     tags: [Users]
+     *     summary: Login user
+     *     description: Log-in an user.
+     *     responses:
+     *       201:
+     *         description: Return the token` and the user role. 
+     *       500:
+     *         description: Server internal error.
+     */
+     server.route('/login')
+        .post(userController.login);
+
+    /**
+     * @openapi
+     * /users/create:
+     *   post:
+     *     tags: [Users]
+     *     summary: Subscribe an user
+     *     description: Subscription for a new user. 
+     *     responses:
+     *       201:
+     *         description: Successfully created a new user.
+     *       500:
+     *         description: Server internal error.
+     */
+    
+    server.route('/users/create')
+            .post(userController.signup);
+
+    /**
+     * @openapi
      * /users:
      *   get:
      *     tags: [Users]
@@ -19,6 +52,22 @@ module.exports = (server) => {
 
     /**
      * @openapi
+     * /users/current:
+     *   get:
+     *     tags: [Users]
+     *     summary: Get the current logged-in user
+     *     description: Get the current logged-in user
+     *     responses:
+     *       200:
+     *         description: Return the logged-in user.
+     *       500:
+     *         description: Server internal error.
+     */
+     server.route('/user')
+        .get(userController.get_current_user);
+
+    /**
+     * @openapi
      * /users/{userId}:
      *   get:
      *     tags: [Users]
@@ -32,22 +81,6 @@ module.exports = (server) => {
      */
     server.route('/users/:userId')
         .get(userController.get_one_user);
-
-    /**
-     * @openapi
-     * /users/{userId}:
-     *   get:
-     *     tags: [Users]
-     *     summary: Get the current logged-in user
-     *     description: Get the current logged-in user
-     *     responses:
-     *       200:
-     *         description: Return the logged-in user.
-     *       500:
-     *         description: Server internal error.
-     */
-    server.route('/users/current')
-        .get(userController.get_current_user);
 
     /**
      * @openapi
@@ -81,35 +114,4 @@ module.exports = (server) => {
     server.route('/users/:userId/delete')
         .delete(userController.delete_self);
 
-    /**
-     * @openapi
-     * /login:
-     *   post:
-     *     tags: [Users]
-     *     summary: Login user
-     *     description: Log-in an user.
-     *     responses:
-     *       201:
-     *         description: Return the token` and the user role. 
-     *       500:
-     *         description: Server internal error.
-     */
-    server.route('/login')
-        .post(userController.login);
-
-    /**
-     * @openapi
-     * /users/create:
-     *   post:
-     *     tags: [Users]
-     *     summary: Subscribe an user
-     *     description: Subscription for a new user. 
-     *     responses:
-     *       201:
-     *         description: Successfully created a new user.
-     *       500:
-     *         description: Server internal error.
-     */
-    server.route('/users/create')
-            .post(userController.signup);
 }
